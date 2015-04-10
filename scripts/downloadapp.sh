d97f434b7acfa9ff533db7deb8f485398156e91d
sengottaiyank <kavinkumar.sengottaiyan@cognizant.com>
2015-04-10 11:45:45 +0530

    updated

--- scripts/downloadapp.sh
+++
@@ -1,2 +0,0 @@
-# Need to add the application download part
-aws s3 cp s3://kavin-codedeploy/sample.war ~/sample.war
--- scripts/stoptomcat6.sh
+++
@@ -1,3 +0,0 @@
-service tomcat6 stop
-# Remove old files
-rm -rf /usr/share/tomcat6/webapps/sample*
